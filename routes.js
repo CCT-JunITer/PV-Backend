de437cf2c729c807e6a9b417a4d1dcea5704690f
Constantin Eissler <constantineissler@MBP-Windows.fritz.box>
2018-08-15 14:12:12 +0200

    added docker environment

--- routes.js
+++ routes.js
@@ -1,19 +1,34 @@
 
 module.exports = (app, db) => {
   app.get('/', (req, res) => {
+    console.log('DOCKER LIVE')
     db.collection('pvdata').find({}).toArray((err, item) => {
       if (err) {
         console.log(err)
         res.send({'error':'An error has occurred'})
       } else {
-        res.send({
-          general1: item[0].general,
-          general2: item[0].general,
-          project1: item[0].projects,
-          project2: item[0].projects,
-          seminar1: item[0].seminars,
-          seminar2: item[0].seminars
-        })
+        if(item[0]) {
+          res.send({
+            general1: item[0].general,
+            general2: item[0].general,
+            project1: item[0].projects,
+            project2: item[0].projects,
+            seminar1: item[0].seminars,
+            seminar2: item[0].seminars
+          })
+        }
+        else {
+          res.send({
+            general1: {},
+            general2: {},
+            project1: {},
+            project2: {},
+            seminar1: {},
+            seminar2: {}
+          })
+        }
+
+
       }
     })
   })
--- server.js
+++ server.js
@@ -11,18 +11,18 @@ const app            = express()
 app.use((req, res, next) => {
   // Website you wish to allow to connect for testing: localhost
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-  next()
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+next()
 })
 app.use(express.json())
 
-const port = 8000
+const port = 8080
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
   require('./routes')(app, database.db("PV-DB"));
-  app.listen(port, () => {
-    console.log('We are live on ' + port + ' with DB: ' + db.url);
-  });
+app.listen(port, () => {
+  console.log('We are live on ' + port + ' with DB: ' + db.url);
+});
 })
